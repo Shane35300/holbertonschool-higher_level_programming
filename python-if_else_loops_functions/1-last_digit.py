15aeae0a645f97cbe9c7209ac6a1532ec69f6a68
shane <6874@holbertonstudents.com>
2023-09-25 15:01:18 +0200

    adding the function task 1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = None
+comp5_6 = None
+if number < 0:
+    last_digit = -number % 10
+else:
+    last_digit = number % 10
+if last_digit < 6 and last_digit != 0:
+    comp5_6 = "and is less than 6 and not 0"
+elif last_digit > 5:
+    comp5_6 = "and is greater than 5 and not 0"
+else:
+    comp5_6 = "and is 0"
+
+print(f"Last digit of {number} is {last_digit} {comp5_6}")
