ff7cfa6b862e002ff6d981a2d70f3b2803078713
shane <6874@holbertonstudents.com>
2023-09-27 14:44:26 +0200

    removing unusefull lines task 1 p 2

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = None
-comp5_6 = None
 if number < 0:
     last_digit = -number % 10
 else:
@@ -13,5 +11,4 @@ elif last_digit > 5:
     comp5_6 = "and is greater than 5"
 else:
     comp5_6 = "and is 0"
-
 print("Last digit of {} is {} {}".format(number, last_digit, comp5_6))
