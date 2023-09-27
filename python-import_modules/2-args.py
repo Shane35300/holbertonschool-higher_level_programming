09096aaf8fa9e11e8f34a262a185bac38b85fbd3
shane <6874@holbertonstudents.com>
2023-09-27 09:33:51 +0200

    adding task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    nb = len(sys.argv) - 1
+    print("{} ".format(nb), end="")
+    print("arguments" if nb != 1 else "argument", end="")
+    print(":" if nb != 0 else ".")
+    for i in range(0,nb):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
