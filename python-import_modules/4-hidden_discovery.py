57b58fae0ff32a0667e62d45b7058f0533749d52
shane <6874@holbertonstudents.com>
2023-10-02 09:39:51 +0200

    adding task 0 p 1

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    mod_names = dir(hidden_4)
-    filtered_names = sorted([name for name in mod_names
-                             if not name.startswith("__")])
-    for name in filtered_names:
-        print(name)
+    string = dir(hidden_4)
+filtered = sorted([name for name in string if not name.startswith("__")])
+for name in filtered:
+    print(name)
