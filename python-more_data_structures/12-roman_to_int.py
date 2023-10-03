2d126965c663913973d34205b0f7534c5ddc3337
shane <6874@holbertonstudents.com>
2023-10-03 15:41:22 +0200

    adding task 12 p 2

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dico = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    i = 0
+    while i < len(roman_string):
+        if i < len(roman_string) - 1:
+            if dico[roman_string[i]] < dico[roman_string[i + 1]]:
+                result += (dico[roman_string[i + 1]] - dico[roman_string[i]])
+                i += 1
+            else:
+                result += dico[roman_string[i]]
+        else:
+            result += dico[roman_string[i]]
+        i += 1
+    return result
