9f619e097d6c295759b3f02feac455da5bb1c5d8
shane <6874@holbertonstudents.com>
2023-10-04 14:34:54 +0200

    adding task 13 p 2

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    result = 0
+    div = 0
+    for i in range(len(my_list)):
+        result += my_list[i][0] * my_list[i][1]
+        div += my_list[i][1]
+    return result / div
