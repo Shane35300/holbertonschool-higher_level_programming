de23af17930fefc15bc18dcec566446cb7bd9bb2
shane <6874@holbertonstudents.com>
2023-10-10 14:14:26 +0200

    correcting task 0 p 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     """:param a: The first number.
     :param b: The second number (default is 98).
     :return: The sum of a and b as an integer."""
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     var_a = int(a)
     var_b = int(b)
