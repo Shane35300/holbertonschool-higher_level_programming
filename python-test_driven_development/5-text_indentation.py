695d7a209bd7c80a68ce0f90aae87e37da975ecd
shane <6874@holbertonstudents.com>
2023-10-10 20:43:49 +0200

    adding task 4 p 2

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This function  prints a text with 2 new lines
+ after each of those characters:
+   .? and :
+    there must be 2 new lines after each splitting
+"""
+
+
+def text_indentation(text):
+    """
+    text is the text that I print
+    separators = ".", ":", "?"
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        print(text[i], end="")
+        if text[i] in ".:?":
+            print("\n")
