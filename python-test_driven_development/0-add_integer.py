d51bc34a999cf4ef0a1fe7cb8024fc429e4e5c84
shane <6874@holbertonstudents.com>
2023-10-10 14:09:48 +0200

    correcting task 0 p 1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""
+>>> add_integer(100, -2)
+98
+>>> add_integer(2)
+100"""
+
+
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats and returns the result as an integer.
-
-    :param a: The first number.
+    """:param a: The first number.
     :param b: The second number (default is 98).
-    :return: The sum of a and b as an integer.
-
-    This function takes two arguments, a and b, which can be integers or
-    floats. It adds these two numbers together and returns the result as
-    an integer. If a or b is not a number (i.e., not an int or a float),
-    a TypeError is raised.
-    """
+    :return: The sum of a and b as an integer."""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
