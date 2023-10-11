3ad246ae293a64c9e98ef9ce6509dad9b071fd4f
shane <6874@holbertonstudents.com>
2023-10-11 09:53:46 +0200

    blkline task 5 p2

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,8 +4,8 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
+
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 10, 12, 9]), 12)
         self.assertEqual(max_integer([-20, -23, -9]), -9)
