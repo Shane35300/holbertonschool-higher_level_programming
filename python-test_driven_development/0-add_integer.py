6a49bfae037dc4bdd8a70384c0af504f55af2815
shane <6874@holbertonstudents.com>
2023-10-13 09:43:30 +0200

    ajidijiji

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,6 +10,8 @@ def add_integer(a, b=98):
     """:param a: The first number.
     :param b: The second number (default is 98).
     :return: The sum of a and b as an integer."""
+    if a is None:
+        raise TypeError("a must be an integer")
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
