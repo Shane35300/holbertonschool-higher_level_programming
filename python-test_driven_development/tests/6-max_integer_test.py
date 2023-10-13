37c023f2ee017076548adf1a56b67a6acd1e5f9c
shane <6874@holbertonstudents.com>
2023-10-13 11:58:36 +0200

    eeeeee

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,11 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 10, 12, 9]), 12)
         self.assertEqual(max_integer([-20, -23, -9]), -9)
         self.assertEqual(max_integer([1, 10, 12, 9]), 12)
+        self.assertEqual(max_integer([20, 19, 12, 9]), 20)
+        self.assertEqual(max_integer([2.2, 1.2, 12.4, 9]), 12.4)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([20, 19, 12, 9]), 20)
+        self.assertEqual(max_integer([]), None)
 
 
 if __name__ == '__main__':
