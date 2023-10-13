02d6c3157a9787503856c717b1df2b5249e096a1
shane <6874@holbertonstudents.com>
2023-10-13 10:56:37 +0200

    space

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,7 +11,7 @@ def say_my_name(first_name, last_name=""):
     :parameter first_name: is the first name
     :parameter last_name: is the last name
     """
-    if not (isinstance(first_name, str) and len(first_name) != 0):
+    if not (isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
