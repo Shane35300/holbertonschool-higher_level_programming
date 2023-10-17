8523dd4f842f3ef6c6aeb4231ab1b7e5895ee622
shane <6874@holbertonstudents.com>
2023-10-17 11:47:06 +0200

    adding task 1 p 2

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a simple Python script that defines a class called MyList.
+MyList is a subclass of the built-in list class and has an additional method
+to print a sorted version of the list.
+"""
+
+
+class MyList(list):
+    """
+    MyList is a subclass of the list class,
+    inheriting its properties and methods.
+    """
+    def print_sorted(self):
+        """
+        This method sorts the elements in the list
+        and prints the sorted list.
+        """
+        print(sorted(self))
