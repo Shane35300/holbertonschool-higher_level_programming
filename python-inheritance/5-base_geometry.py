1f9cfb8a02fa2693677e329c5dc956817627b568
shane <6874@holbertonstudents.com>
2023-10-17 16:34:36 +0200

    ajout de commentaire task 5 p 2

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -5,4 +5,10 @@ BaseGeometry is a empty class
 
 
 class BaseGeometry():
+    """
+    Cette classe représente une figure géométrique.
+
+    Il n'a pas encore d'attributs ou de méthodes spécifiques.
+    """
+
     pass
