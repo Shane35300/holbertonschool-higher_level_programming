b350044b1f9664727c4d9b05cd31b83aad1c6201
shane <6874@holbertonstudents.com>
2023-10-17 15:56:38 +0200

    adding task 4 p 2

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this function return true if "obj" class is the same
+as a_class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    obj is the reference
+    a_class is the class"""
+    if not type(obj) is a_class:
+        if issubclass(type(obj), a_class):
+            return True
+    else:
+        return False
