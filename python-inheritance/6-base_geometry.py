8097a598edba71b66a965e56812dcdfac72eb04e
shane <6874@holbertonstudents.com>
2023-10-17 16:41:01 +0200

    add task 6 p 2

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+BaseGeometry is a empty class
+"""
+
+
+class BaseGeometry():
+    """
+    Cette classe représente une figure géométrique.
+
+    Il n'a pas encore d'attributs ou de méthodes spécifiques.
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
