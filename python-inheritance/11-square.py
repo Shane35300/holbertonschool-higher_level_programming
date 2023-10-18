e4a561982ae92032a6b07bd9c824c0f8664cd8b4
shane <6874@holbertonstudents.com>
2023-10-18 11:14:22 +0200

    add 11 p 2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+BaseGeometry is a empty class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Cette classe représente un carré.
+
+    Il n'a pas encore d'attributs ou de méthodes spécifiques.
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        string = f"[Square] {self.__width}/{self.__height}"
+        return string
