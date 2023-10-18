74c95d8036e1d79c93902adca73ac7db2d0fbe23
shane <6874@holbertonstudents.com>
2023-10-18 10:09:29 +0200

    8p2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -28,8 +28,14 @@ class BaseGeometry():
 
 
 class Rectangle(BaseGeometry):
+    """
+    Cette classe représente un rectangle.
+
+    Il n'a pas encore d'attributs ou de méthodes spécifiques.
+    """
+
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
