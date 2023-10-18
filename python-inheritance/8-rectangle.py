f11867306e8fe99eba0530e9b245a2c62e07226f
shane <6874@holbertonstudents.com>
2023-10-18 10:10:48 +0200

    add import 7 task 8 p 2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,28 +4,7 @@ BaseGeometry is a empty class
 """
 
 
-class BaseGeometry():
-    """
-    Cette classe représente une figure géométrique.
-
-    Il n'a pas encore d'attributs ou de méthodes spécifiques.
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is bool:
-            raise TypeError("{} must be an integer".format(name))
-        if name is None:
-            raise TypeError("{} must be an integer".format(name))
-        if not type(value) is int and not type(value) is float:
-            raise TypeError("{} must be an integer".format(name))
-        if type(value) is float:
-            value = int(value)
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
