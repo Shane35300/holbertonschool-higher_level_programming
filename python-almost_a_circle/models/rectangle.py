17844c95d9f3a121a90c59a5e7c5beb92cace14b
shane <6874@holbertonstudents.com>
2023-10-23 11:08:18 +0200

    adding task 2 p 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+"""
+This file contains the definition of the Rectangle class,
+which inherits from the Base class.
+It allows for the representation of a rectangle with attributes
+for width, height, X position, and Y position.
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    This class defines a rectangle by inheriting from the Base class.
+    It represents a rectangle with attributes for width,
+    height, X position, and Y position.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+         Constructor for the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The X position of the rectangle. Default is 0.
+            y (int, optional): The Y position of the rectangle. Default is 0.
+            id (int, optional): The rectangle's identifier. Default is None.
+
+        Raises:
+            TypeError: If any of the inputs is not an integer.
+            ValueError: If any of the inputs is less than 0.
+        """
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__x
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 class TestBase(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
     def test_id_assignment(self):
         # Créez une instance de la classe Base
         instance1 = Base()
@@ -13,6 +17,59 @@ class TestBase(unittest.TestCase):
         self.assertEqual(instance1.id, 1)
         self.assertEqual(instance2.id, 2)
         self.assertEqual(instance3.id, 12)
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_id_assignment(self):
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 12)
+
+    def test_invalid_width(self):
+        # Test d'une largeur invalide (doit lever une exception TypeError)
+        with self.assertRaises(TypeError):
+            r = Rectangle("invalid", 5)
+
+    def test_negative_width(self):
+        # Test d'une largeur négative (doit lever une exception ValueError)
+        with self.assertRaises(ValueError):
+            r = Rectangle(-5, 5)
+
+    def test_invalid_height(self):
+        # Test d'une hauteur invalide (doit lever une exception TypeError)
+        with self.assertRaises(TypeError):
+            r = Rectangle(5, "invalid")
+
+    def test_negative_height(self):
+        # Test d'une hauteur négative (doit lever une exception ValueError)
+        with self.assertRaises(ValueError):
+            r = Rectangle(5, -5)
+
+    def test_invalid_x(self):
+        # Test d'une position X invalide (doit lever une exception TypeError)
+        with self.assertRaises(TypeError):
+            r = Rectangle(5, 5, "invalid", 0)
+
+    def test_negative_x(self):
+        # Test d'une position X négative (doit lever une exception ValueError)
+        with self.assertRaises(ValueError):
+            r = Rectangle(5, 5, -5, 0)
+
+    def test_invalid_y(self):
+        # Test d'une position Y invalide (doit lever une exception TypeError)
+        with self.assertRaises(TypeError):
+            r = Rectangle(5, 5, 0, "invalid")
+
+    def test_negative_y(self):
+        # Test d'une position Y négative (doit lever une exception ValueError)
+        with self.assertRaises(ValueError):
+            r = Rectangle(5, 5, 0, -5)
 
 if __name__ == '__main__':
     unittest.main()
