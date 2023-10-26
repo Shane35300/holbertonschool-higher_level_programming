c3e1d2eb617a96f709aa52b263af7e7bf613711a
shane <6874@holbertonstudents.com>
2023-10-26 10:57:54 +0200

    zzzz

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,17 +17,17 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        return self.size
+        return self.__size
 
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-        self.size = self.width
+        self.__size = self.width
 
     def __str__(self):
         """
