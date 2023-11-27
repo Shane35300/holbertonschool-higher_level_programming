c2c23d3000dbc7ce712999bbed6cd135e1d5f4a9
shane <6874@holbertonstudents.com>
2023-11-27 10:37:15 +0100

    add task 11 p 2

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Create the connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], "", sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Create a new State object for Louisiana
+    louisiana = State(name="Louisiana")
+
+    # Add the Louisiana object to the session
+    session.add(louisiana)
+
+    # Commit the changes to the database
+    session.commit()
+
+    # Query all State objects and print the results
+    result = (
+        session.query(State)
+        .filter(State.name.like('Louisiana'))
+        .all()
+    )
+    if result:
+        for line in result:
+            print("{}".format(line.id))
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
