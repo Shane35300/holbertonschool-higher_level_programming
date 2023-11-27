9850b73927b699c199f30f1507a55ff5517ea36e
shane <6874@holbertonstudents.com>
2023-11-27 12:32:18 +0100

    kokoko

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@ Module defining the State class using SQLAlchemy
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -19,5 +18,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    # Définition de la relation entre City et State
-    cities = relationship("City", back_populates="state")
